54340fe14db8d55f05c958c30c1ec8c54ae89189
Martin <mkmartinoes@gmail.com>
2022-05-06 23:23:50 +0300

    Add to cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+// import { useState } from "react/cjs/react.production.min";
 
 function Item({ name, category }) {
+  const [classNames, setclassNames] = useState("")
+  function handleCart(){
+    setclassNames("in-cart")
+  }
   return (
-    <li className="">
+    <li className= {classNames}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>Add to Cart</button>
     </li>
   );
 }
